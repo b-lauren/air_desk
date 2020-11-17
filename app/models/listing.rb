a85640a9eeab08aba8217d0e50b7eaefcb7d35fc
John Joubert <john@robotdwarf.com>
2020-11-17 23:08:28 +0000

    Add  bookings controller for #index #owner_index #new #create

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ApplicationRecord
   has_many_attached :photos
   validates :description, :title, :rate, presence: true
   belongs_to :user
+  has_many :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
-  resources :listings
+
+  resources :listings do
+    resources :bookings, except: [:destroy]
+  end
+
+  get 'bookings', to: 'bookings#owner_index'
+  delete 'bookings/:id', to: 'bookings#destroy'
 end
