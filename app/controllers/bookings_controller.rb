a04264b6bfb2d16fcc0ced816e0b2be296086faa
Alex Price <alexprice009@gmail.com>
2020-11-18 12:55:05 +0000

    show page functional

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,6 +8,11 @@ class BookingsController < ApplicationController
     @owner_bookings = owner_listings.map(&:bookings).flatten
   end
 
+  def show
+    @listing = Listing.find(params[:listing_id])
+    @booking = Booking.find(params[:id])
+  end
+
   def new
     @listing = Listing.find(params['listing_id'])
     @booking = Booking.new
@@ -18,7 +23,7 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.listing = Listing.find(params['listing_id'])
     if @booking.save
-      redirect_to listing_path(@booking.listing)
+      redirect_to listing_booking_path(@booking)
     else
       render 'new'
     end
@@ -33,3 +38,22 @@ class BookingsController < ApplicationController
     )
   end
 end
+
+                 #                 root GET    /                                                                                        pages#home
+                 #     listing_bookings GET    /listings/:listing_id/bookings(.:format)                                                 bookings#index
+                 #                      POST   /listings/:listing_id/bookings(.:format)                                                 bookings#create
+                 #  new_listing_booking GET    /listings/:listing_id/bookings/new(.:format)                                             bookings#new
+                 # edit_listing_booking GET    /listings/:listing_id/bookings/:id/edit(.:format)                                        bookings#edit
+                 #      listing_booking GET    /listings/:listing_id/bookings/:id(.:format)                                             bookings#show
+                 #                      PATCH  /listings/:listing_id/bookings/:id(.:format)                                             bookings#update
+                 #                      PUT    /listings/:listing_id/bookings/:id(.:format)                                             bookings#update
+                 #             listings GET    /listings(.:format)                                                                      listings#index
+                 #                      POST   /listings(.:format)                                                                      listings#create
+                 #          new_listing GET    /listings/new(.:format)                                                                  listings#new
+                 #         edit_listing GET    /listings/:id/edit(.:format)                                                             listings#edit
+                 #              listing GET    /listings/:id(.:format)                                                                  listings#show
+                 #                      PATCH  /listings/:id(.:format)                                                                  listings#update
+                 #                      PUT    /listings/:id(.:format)                                                                  listings#update
+                 #                      DELETE /listings/:id(.:format)                                                                  listings#destroy
+                 #             bookings GET    /bookings(.:format)                                                                      bookings#owner_index
+                 #                      DELETE /bookings/:id(.:format)
