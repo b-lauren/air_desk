81d3ba8f4cea88494e78831c8a097f818df3e902
Rebecca Ross <b.laurenross@gmail.com>
2020-11-20 11:14:47 +0000

    fixed new listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ApplicationRecord
   has_many_attached :photos
-  validates :description, :title, :rate, presence: true
+  validates :description, :title, :rate, :photos, presence: true
   belongs_to :user
   has_many :bookings
   geocoded_by :address_line_1
