7c02d2eef0df93f5724359c8042d94c76c187492
Rebecca Ross <b.laurenross@gmail.com>
2020-11-20 12:05:13 +0000

    working searchbar

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,11 @@ class Listing < ApplicationRecord
   has_many :bookings
   geocoded_by :address_line_1
   after_validation :geocode, if: :will_save_change_to_address_line_1?
+
+   include PgSearch::Model
+  pg_search_scope :search_listings,
+    against: [ :title, :description, :address_line_1 ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
